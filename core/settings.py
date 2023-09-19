3d00683ceb66a222d7fb10687e1aa5516dcc17b1
ChaoLIUqaq <102281706+ChaoLIUqaq@users.noreply.github.com>
2023-09-20 01:25:52 +1000

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -188,3 +188,7 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
